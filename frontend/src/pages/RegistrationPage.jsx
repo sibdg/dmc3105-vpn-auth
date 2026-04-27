07dc05479509344c6b286d10fdf64551ce8768a1
dmc3105 <algovor04@gmail.com>
2026-04-27 08:44:40 +0700

    Remove guide links

--- frontend/src/pages/RegistrationPage.jsx
+++ frontend/src/pages/RegistrationPage.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import GuideLinks from "../components/GuideLinks";
 import RegistrationWizard from "../components/RegistrationWizard";
 
 export default function RegistrationPage({ notify }) {
@@ -17,7 +16,6 @@ export default function RegistrationPage({ notify }) {
   return (
     <>
       <RegistrationWizard notify={notify} onSuccess={handleSuccess} onStepChange={setStep} />
-      {step === 3 && <GuideLinks />}
     </>
   );
 }
